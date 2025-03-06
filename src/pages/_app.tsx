4807cab4a315bd5ed69485fef4fe1a35c6bab4de
Samuele <samuele.bragalini@gmail.com>
2025-03-06 10:56:08 +0100

    Removed Footer and NavBar, updated _app.tsx and _document.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,15 @@
 import '../styles/globals.css'
 
 import type { AppProps } from 'next/app'
-import { NavBar } from '../pages/NavBar'
-import { Footer } from '../pages/Footer';
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (<>
-    <NavBar />
+    
     <div className="flex flex-col items-center justify-center min-w-full">
       <Component {...pageProps} />
     </div>
-    <Footer />
+    
   </>)
 }
 
