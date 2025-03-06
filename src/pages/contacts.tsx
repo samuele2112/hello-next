a7783560b1db12690e611056cebb708578ab953e
Samuele <samuele.bragalini@gmail.com>
2025-03-06 11:33:34 +0100

    add env key

--- src/pages/contacts.tsx
+++ src/pages/contacts.tsx
@@ -4,7 +4,7 @@ function Contatti() {
     return (
         <div className="flex items-center justify-center h-screen">
             <h1 className="text-5xl text-pink-600 font-bold my-5">
-                Contatti
+                Contatti {process.env.NEXT_PUBLIC_CUSTOM_KEY}
             </h1>
         </div>
     );
